409a5a7d27ea202adf0200505286e645917ec227
Gong Meng <18588264128@163.com>
2022-04-08 14:54:12 +0800

    css-modules 配置

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,7 +2,10 @@ module.exports = (isDev) => {
     return {
         preserveWhitespace: true,
         extractCSS: !isDev,
-        cssModules: {}
+        cssModules: {
+            localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+            camelCase: true
+        }
         //hotReload: false, //根据环境变量生成
     }
 }
